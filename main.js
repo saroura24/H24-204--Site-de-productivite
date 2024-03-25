290e969e97fe4edd6467aa7f72f34ec2e0a28bf5
yayusem <yusuf.baran.20005@gmail.com>
2024-03-25 16:22:56 -0400

    Update About Us

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-let hrs = document.getElementById("hrs");
-let min = document.getElementById("min");
-
-setInterval(() => {
-    let heureMtn = new Date();
-
-    if(heureMtn.getHours() < 10){
-        hrs.innerHTML = "0" + heureMtn.getHours();
-    } else{
-        hrs.innerHTML = heureMtn.getHours();
-    }
-
-    if(heureMtn.getMinutes() < 10){
-        min.innerHTML = "0" + heureMtn.getMinutes();
-    } else{
-        min.innerHTML = heureMtn.getMinutes();
-    }
-    
-},1000)
-
-
-
-
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const navbar = document.querySelector('.navbar');
