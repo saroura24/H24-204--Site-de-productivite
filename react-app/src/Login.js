0077ae172c7ea0f8843b31ccc1a8222bd319aa9f
YannToukam <yannyvan2005@gmail.com>
2024-03-27 22:57:56 -0400

    Music-player implémenté dans le code du site
    Trouver comment render le music player React sur le site Express

---
+++ react-app/src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="App">
+            <div className="main-wrapper">
+                <a className="btn-spotify" href="/auth/login" >
+                    Login with Spotify 
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
+
--- src/index.js
+++ src/index.js
@@ -3,109 +3,85 @@ const path = require("path");
 const bcrypt = require("bcrypt");
 const collection = require("./config");
 
-
 const app = express();
 
+// Middleware
 app.use(express.json());
-
 app.use(express.urlencoded({extended: false}));
-
 app.set('view engine', 'ejs');
-
 app.use(express.static("public"));
 
-app.get("/", (req,res) =>{
+// Server-side routes
+app.get("/", (req,res) => {
   res.render("index");
-  });
-
-app.get("/accueil", (req,res) =>{
-  res.render("index");
-  });
-
-app.get("/connexion", (req,res) =>{
-res.render("login");
-});
-
-app.get("/inscription", (req,res) =>{
-
-res.render("signup");
 });
 
 
+app.get("/connexion", (req,res) => {
+  res.render("login");
+});
 
-app.get("/parametres", (req,res) =>{
+app.get("/inscription", (req,res) => {
+  res.render("signup");
+});
+
+app.get("/parametres", (req,res) => {
   res.render("parametres");
-  });
-
-app.get("/pomodoro", (req,res) =>{
-
-res.render("pomodoro");
 });
 
-app.get("/a-propos", (req,res) =>{
-
-res.render("about");
+app.get("/pomodoro", (req,res) => {
+  res.render("pomodoro");
 });
 
-app.get("/bot", (req,res) =>{
+app.get("/a-propos", (req,res) => {
+  res.render("about");
+});
 
+app.get("/bot", (req,res) => {
   res.render("bot");
-  });
+});
 
-
-  app.post("/connexion", async (req,res)=>{
-
-    try {
-
-      const check = await collection.findOne({email: req.body.email});
-     
-      if(!check){
-        res.send("Cet email n'existe pas");
-      }else{
-
+app.post("/connexion", async (req,res) => {
+  try {
+    const check = await collection.findOne({email: req.body.email});
+    if(!check){
+      res.send("Cet email n'existe pas");
+    } else {
       const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
-
       if(isPasswordMatch){
-          res.redirect("/accueil");
-      }else{
+        res.redirect("/accueil");
+      } else {
         res.send("Mauvais mot de passe");
       }
     }
-    }catch{
-        res.send("Erreur");
-
-    }
-
+  } catch {
+    res.send("Erreur");
+  }
 });
 
-app.post("/inscription", async (req,res)=>{
-const user = {
-  name: req.body.name,
-  email: req.body.email,
-  password: req.body.password
-}
+app.post("/inscription", async (req,res) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
 
   const existingUser = await collection.findOne({email: user.email})
 
   if(existingUser){
     res.send("Ce nom d'utilisateur existe déjà");
-  }else{
-
+  } else {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-
-user.password = hashedPassword;
-
+    user.password = hashedPassword;
     const userdata = await collection.insertMany(user);
     console.log(userdata);
     res.redirect("/accueil");
   }
-  
 });
 
-
-
-const port= 5000;
-app.listen(port, ()=>{
-console.log(`Server running on Port: ${port}`);
+// Start the server
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on Port: ${port}`);
 });
